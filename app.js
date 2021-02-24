1c6e4f8d3afea386f9078395c0c3987e4cd44489
AndreiCiobanu <andrei.gabriel9850@gmail.com>
2021-02-24 13:58:10 +0200

    day3

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { User, messagesStructure } = require('./models/messagesStucture');
-
-
 
 
 app.use(bodyParser.json());
@@ -17,105 +14,11 @@ app.get('/', (req, res) => {
     res.send('Homepage test')
 });
 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users)
-    }
-    catch (err) {
-        res.json({ message: err })
-    }
-})
+const usersRoute = require('./routes/users')
+app.use('/users', usersRoute)
 
-app.post('/addUser', async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone
-    })
-    try {
-        const savedUser = await user.save()
-        res.json(savedUser)
-    } catch (err) {
-        res.json({ message: err });
-    }
-});
-
-app.delete('/deleteUser/:userId', async (req, res) => {
-    try {
-        const removedUser = await User.deleteOne({ _id: req.params.userId })
-        res.json(removedUser);
-    } catch (err) {
-        res.json({ message: err })
-
-    }
-    ;
-});
-
-app.put('/updateUser/:userId', async (req, res) => {
-    try {
-        const updatedUser = await User.updateOne(
-            { _id: req.params.userId },
-            { $set: { ...req.body } })
-        res.json(updatedUser)
-    }
-    catch (err) {
-        res.json({ message: err })
-
-    }
-})
-
-// routes for Structure
-
-app.post('/addMessageStructure', async (req, res) => {
-    const newStructure = new messagesStructure({
-        ...req.body
-    })
-    try {
-        const savedStructure = await newStructure.save()
-        res.json(savedStructure)
-    }
-    catch (err) {
-        res.json({ message: err })
-
-    }
-})
-
-app.get('/messageStructures', async (req, res) => {
-    try {
-        const structures = await messagesStructure.find();
-        res.json(structures);
-    }
-    catch (err) {
-        res.json({ message: err })
-    }
-})
-
-app.put('/updateStructure/:structureId', async (req, res) => {
-    try {
-        const updatedStructure = await messagesStructure.updateOne(
-            { _id: req.params.structureId },
-            { $set: { ...req.body } })
-        res.json(updatedStructure)
-    }
-    catch (err) {
-        res.json({ message: { err } })
-
-    }
-})
-
-app.delete('/deleteStructure/:structureId', async (req, res) => {
-    try {
-        const removedStructure = await messagesStructure.deleteOne({ _id: req.params.structureId })
-        res.json(removedStructure)
-
-    }
-    catch (err) {
-        res.json({ message: err })
-
-    }
-})
-
+const structureRoute = require('./routes/msgStructure')
+app.use('/msgStructure', structureRoute)
 
 
 app.listen(3000);
---
+++ routes/msgStructure.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const {messagesStructure } = require('../models/messagesStucture');
+const fs = require('fs')
+
+router.post('/addMessageStructure', async (req, res) => {
+    const newStructure = new messagesStructure({
+        ...req.body
+    })
+    try {
+        const savedStructure = await newStructure.save()
+        res.json(savedStructure)
+    }
+    catch (err) {
+        res.json({ message: err })
+
+    }
+})
+
+router.get('/allMessageStructures', async (req, res) => {
+    try {
+        const structures = await messagesStructure.find();
+        res.json(structures);
+    }
+    catch (err) {
+        res.json({ message: err })
+    }
+})
+
+router.put('/updateStructure/:structureId', async (req, res) => {
+    try {
+        const updatedStructure = await messagesStructure.updateOne(
+            { _id: req.params.structureId },
+            { $set: { ...req.body } })
+        res.json(updatedStructure)
+    }
+    catch (err) {
+        res.json({ message: { err } })
+
+    }
+})
+
+router.delete('/deleteStructure/:structureId', async (req, res) => {
+    try {
+        const removedStructure = await messagesStructure.deleteOne({ _id: req.params.structureId })
+        res.json(removedStructure)
+
+    }
+    catch (err) {
+        res.json({ message: err })
+
+    }
+})
+
+
+router.get('/specificStucture/:structureId/sorted', async(req, res) => {
+    try {
+        const id = mongoose.Types.ObjectId(req.params.structureId)
+        console.log(id)
+        const specificStructure =  await messagesStructure.aggregate([
+            {"$match" : {_id : id}},
+          { "$unwind" : "$messages" },
+          { "$sort" : {
+              "messages.date" : -1
+          }
+        },
+        {"$group" : {
+            "_id" : "$_id",
+            
+
+            "messages": {
+                "$push": "$messages"
+            }
+        }}
+
+        ])
+
+        let data = JSON.stringify(specificStructure)
+        fs.writeFileSync('messages-sorted.json', data)
+        
+         res.json(specificStructure)
+        
+
+
+    }
+    catch(err){
+        res.json({message:err})
+
+    }
+})
+
+
+
+
+module.exports = router;
